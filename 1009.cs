2eb8ebe07e62ebf5c500030e87e2add3227bc247
Maria Idalina <mridalina@gmail.com>
2022-05-16 05:32:37 -0300

    1009

---
+++ 1009.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Q1_1009
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string nomeVendedor = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine());
+            double montante = double.Parse(Console.ReadLine());
+
+            double salarioTotal = montante * 0.15 + salarioFixo;
+
+            Console.WriteLine($"TOTAL = R$ {salarioTotal:F2}");
+
+        }
+
+    }
+}
+    }
+}

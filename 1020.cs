d9a94ed84adc78742b1c984c0901146a93e39e10
Maria Idalina <mridalina@gmail.com>
2022-05-16 05:48:31 -0300

    1020

---
+++ 1020.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6_1020
+{
+    class Program
+    {
+        static void Main(string[] args) {
+           
+
+                int idade_dias = int.Parse(Console.ReadLine());
+
+                int ano = idade_dias / 365;
+                int mes = idade_dias % 365 / 30;
+                int dia = (idade_dias % 365) % 30;
+
+                Console.Write("{0} ano(s)\n{1} mes(es)\n{2} dia(s)\n", ano, mes, dia);
+
+        }
+    }
+}
+    
+
